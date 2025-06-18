e31fea5ed73397aefbeb34338cd4b9462c9ff5fd
Jang-GO <jang8195@naver.com>
2025-06-18 09:21:48 +0900

    chore: Spring AI 의존성 추가

---
+++ src/main/java/com/archiservice/common/config/AIConfig.java
@@ -0,0 +1,9 @@
+package com.archiservice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AIConfig {
+
+}

887df154eca83c9619957e27f40c808de77a79a1
1seyoung <akdlrnal0705@gmail.com>
2025-06-20 13:10:50 +0900

    chore : Example 패키지 제거

--- src/main/java/com/archiservice/example/service/ExampleService.java
+++
@@ -1,9 +0,0 @@
-package com.archiservice.example.service;
-
-import com.archiservice.example.dto.response.ExampleResponseDto;
-
-public interface ExampleService {
-  ExampleResponseDto getSuccessExample();
-  ExampleResponseDto getCustomErrorExample();
-  ExampleResponseDto getUnhandledErrorExample();
-}

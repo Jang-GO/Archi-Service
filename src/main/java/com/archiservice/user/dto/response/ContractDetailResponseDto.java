32a1f90310aebb191aa50f8fdffb3ed5389aba4a
guntaek <rjsxor7584@naver.com>
2025-06-22 22:35:29 +0900

    fix : ContractDetailResponseDto vasPrice 타입 변경

--- src/main/java/com/archiservice/user/dto/response/ContractDetailResponseDto.java
+++ src/main/java/com/archiservice/user/dto/response/ContractDetailResponseDto.java
@@ -1,12 +1,17 @@
 package com.archiservice.user.dto.response;
 
+import com.archiservice.product.vas.domain.Money;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContractDetailResponseDto {
     private String paymentMethod;
     private long contractPrice;
@@ -21,7 +26,7 @@ public class ContractDetailResponseDto {
     private String messageUsage;
 
     private String vasName;
-    private Integer vasPrice;
+    private Money vasPrice;
     private String vasCategory;
     private String vasDescription;
     private Integer saleRate;
